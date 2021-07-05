b7483fbd5cbd681b678335965188fdb56b1f961c
M9 <m9@L65071281.acad.unifor.br>
2021-07-05 15:21:53 -0300

    alterando endereco proxy

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const proxy = require('express-http-proxy')
 const app = express()
 
-app.use('/proxy', proxy('http://172.18.9.71:6001/'))
 
-app.listen('6001', async (req, res) => {
-    console.log('Servidor rodando na 6001');
+
+app.use('/proxy', proxy('172.18.9.71:3000/'));
+
+
+app.listen('6002', async (req, res) => {
+    console.log('Servidor rodando na 6002');
 })
 
